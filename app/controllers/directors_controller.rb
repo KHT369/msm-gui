e785ff51a623b4f5208f7fc2f63640c4cc7d42b7
KHT369 <ktsao@chicagobooth.edu>
2024-11-13 17:37:21 +0000

    save 1

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,4 +1,62 @@
 class DirectorsController < ApplicationController
+  def update
+
+    # Get the ID out of params
+    d_id = params.fetch("the_id")
+
+    # Look up the existing record
+    matching_records = Director.where({ :id => d_id })
+
+    the_director = matching_records.at(0)
+
+    # Overwrite each column with the values from user inputs
+    the_director.name = params.fetch("director_name")
+
+    the_director.dob = params.fetch("director_dob")
+
+    the_director.bio = params.fetch("director_bio")
+
+    the_director.image = params.fetch("director_image")
+    # Save
+
+    the_director.save
+
+    # Redirect to the movie details page
+    redirect_to("/directors/#{the_director.id}")
+  end
+
+  def new_director
+    @director_name = params.fetch("director_name")
+    @director_dob = params.fetch("director_dob")
+    @director_bio = params.fetch("director_bio")
+    @director_image = params.fetch("director_image")
+
+    director_n = Director.new
+    director_n.name = @director_name
+    director_n.dob = @director_dob
+    director_n.bio = @director_bio
+    director_n.image = @director_image
+
+    director_n.save
+
+    redirect_to("/directors")
+  end
+
+  def destroy
+
+    the_id = params.fetch("an_id")
+
+    matching_records = Director.where({ :id => the_id })
+
+    the_director = matching_records.at(0)
+
+    the_director.destroy
+
+    redirect_to("/directors")
+  end
+  
+  
+  
   def index
     matching_directors = Director.all
     @list_of_directors = matching_directors.order({ :created_at => :desc })
@@ -35,17 +93,5 @@ class DirectorsController < ApplicationController
     @eldest = directors_by_dob_asc.at(0)
 
     render({ :template => "director_templates/eldest" })
-  end
-  
-  def new_director
-    @director_name = params.fetch("director_name")
-    @director_dob = params.fetch("director_dob")
-    @director_bio = params.fetch("director_bio")
-    @director_image = params.fetch("director_image")
-
-    director_n = Director.new
-    director
-
-    render({ :template => "director_templates/index" })
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,9 +11,19 @@ Rails.application.routes.draw do
 
   post("/insert_director", { :controller => "directors", :action => "new_director" })
 
+  get("/delete_director/:an_id", { :controller => "directors", :action => "destroy" })
+
+  post("/modify_director_record/:the_id", { :controller => "directors", :action => "update" })
+
   get("/movies", { :controller => "movies", :action => "index" })
 
   get("/movies/:path_id", { :controller => "movies", :action => "show" })
+
+  post("/insert_movie_record", { :controller => "movies", :action => "create" })
+
+  get("/delete_movie/:an_id", { :controller => "movies", :action => "destroy" })
+
+  post("/modify_movie_record/:the_id", { :controller => "movies", :action => "update" })
   
   get("/actors", { :controller => "actors", :action => "index" })
 
